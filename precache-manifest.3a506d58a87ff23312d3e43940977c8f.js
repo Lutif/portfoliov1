e779cf07a574743e695f71251c4e15efe2bd52f4
Lutif <lmandhro@gmail.com>
2019-12-23 21:58:15 +0500

    Updates

---
+++ precache-manifest.3a506d58a87ff23312d3e43940977c8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc8dba4fdd0da041985b5d4bf6e1545",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9b1dd6b7395b284ae952",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "befc16f737b15a6ceb5e",
+    "url": "/portfolio/static/js/2.bb33a59f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.bb33a59f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9b1dd6b7395b284ae952",
+    "url": "/portfolio/static/js/main.b8b09b12.chunk.js"
+  },
+  {
+    "revision": "8575fab3f3395c90d845",
+    "url": "/portfolio/static/js/runtime-main.680679b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
